7e64e865330add1398eafaf427ecdea0e445da79
titov-andrei <titov_andrei@mail.ru>
2021-01-22 14:07:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Lesson from "./02_components/Lesson.jsx";
 
-ReactDOM.render(<Lesson />, document.getElementById("root"));
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
