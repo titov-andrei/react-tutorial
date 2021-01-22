41497ac4a7418b8d17f6d9b33d446f3f8f25b13d
titov-andrei <titov_andrei@mail.ru>
2021-01-22 15:35:54 +0300

    Update Lesson.jsx

---
+++ src/02_components/Lesson.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome() {
+  return <h1>Hello world!</h1>
+}
+
+class SuperWelcome extends React.Component {
+  render() {
+    return <h1>Hello world!</h1>
+  }
+}
