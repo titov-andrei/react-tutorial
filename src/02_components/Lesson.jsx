4f6f30767f37950b9c006b2ac4c5e5fce705d32c
titov-andrei <titov_andrei@mail.ru>
2021-01-24 22:30:09 +0300

    Update Lesson.jsx

--- src/02_components/Lesson.jsx
+++ src/02_components/Lesson.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Welcome() {
-  return <h1>Hello world!</h1>
+  return <h1>Hello world!</h1>;
 }
 
 class SuperWelcome extends React.Component {
   render() {
-    return <h1>Hello world!</h1>
+    return <h1>Hello world!</h1>;
   }
 }
+
+function Lesson() {
+  return (
+    <div>
+      <Welcome />
+      <SuperWelcome />
+    </div>
+  );
+}
+
+export default Lesson;
