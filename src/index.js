fd0bd92d588901daefa5f98faf2ca7e37b2e02ed
titov-andrei <titov_andrei@mail.ru>
2021-01-25 12:35:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import Lesson from "./02_components/Lesson.jsx";
-import Lesson from "./03_state/Lesson.jsx";
+// import Lesson from "./03_state/Lesson.jsx";
+import Lesson from "./04_props/Lesson.jsx";
 
 ReactDOM.render(<Lesson />, document.getElementById("root"));
