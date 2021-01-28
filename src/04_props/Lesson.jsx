2dfa844c94541e43312d01d3374217ec4fd9c279
titov-andrei <titov_andrei@mail.ru>
2021-01-28 14:12:39 +0300

    Update Lesson.jsx

--- src/04_props/Lesson.jsx
+++ src/04_props/Lesson.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 const Counter = ({ counter, func, number, string }) => {
   console.log(counter, func, number, string);
   return <h1>{`Counter component. Counter value is: ${counter}`}</h1>;
 };
+
+Counter.PropTypes = {
+  counter: PropTypes.number,
+  func: PropTypes.func,
+  number: PropTypes.number,
+  string: PropTypes.string,
+}
 
 class CounterButton extends Component {
   state = {
