db74cae503648f5451f00c6e9b8f925a0eb4bb2f
titov-andrei <titov_andrei@mail.ru>
2021-02-03 11:28:51 +0300

    Update Lesson.jsx

--- src/05_props/Lesson.jsx
+++ src/05_props/Lesson.jsx
@@ -4,6 +4,37 @@ import PropTypes from 'prop-types';
 // PropTypes
 
 // Node:
-Component.propTypes = {
-  node: PropTypes.node,
+// Component.propTypes = {
+//   node: PropTypes.node,
+// }
+
+export const Counter = ({ counter = 0 }) => {
+  return <h1>{`Counter component. Counter value is: ${counter}`}</h1>
+}
+
+export class Lesson extends Component {
+  state = {
+    counter: 0,
+  }
+
+  handleClick = () => {
+    this.setState(({ counter }) => ({
+      counter: ++counter,
+    }))
+  }
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <div>{counter}</div>
+        <Counter
+          counter={counter}
+        />
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    )
+  }
+
 }
