bd4dde9ee5dfd6aa2d4b479d6ccb8b49574ae8f0
titov-andrei <titov_andrei@mail.ru>
2021-02-09 09:43:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 // import Lesson from "./02_components/Lesson.jsx";
 // import Lesson from "./03_state/Lesson.jsx";
 // import Lesson from "./04_props/Lesson.jsx";
-import { Lesson, Counter, Button } from "./05_props/Lesson.jsx";
+// import { Lesson, Counter, Button } from "./05_props/Lesson.jsx";
+import Lesson from "./06_events/Lesson.jsx";
 
-ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById("root"));
+// ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById("root"));
+
+
