ab4e714c6e6c457cbcdd144d883cea1f763306d3
titov-andrei <titov_andrei@mail.ru>
2021-02-11 10:24:51 +0300

    Update Lesson.jsx

--- src/06_events/Lesson.jsx
+++ src/06_events/Lesson.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 
 const MyLink = ({ text, onClick }) => (
-  
-)
+  <a href="/test" onClick={onClick}>
+    {text}
+  </a>
+);
+
+MyLink.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+MyLink.defaultProps = {
+  text: 'link',
+  onClick: () => {},
+}
 
 class App extends Component {
   render() {
