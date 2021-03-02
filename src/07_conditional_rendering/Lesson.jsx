f839457bc5063383671885f594fe446ab9faeba9
titov-andrei <titov_andrei@mail.ru>
2021-03-02 07:45:12 +0300

    Update Lesson.jsx

--- src/07_conditional_rendering/Lesson.jsx
+++ src/07_conditional_rendering/Lesson.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+const ValidationMsg = ({ val }) => {
+  if (val >= 10) {
+    return <h2>Crate than 10</h2>;
+  } else {
+    return (
+      <h3>
+        Less than <em>10</em>
+      </h3>
+    );
+  }
+};
+
 const App = () => {
-  return <div>!!!</div>;
+  return <ValidationMsg val={8} />;
 };
 
 export default App;
